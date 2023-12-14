5435fcded2244138a722d8c186b7702fb91405b2
mxnuchim <manuchimoliver779@gmail.com>
2023-12-14 09:24:21 +0100

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Manuchim Oliver on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
